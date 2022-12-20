d0074697edfaa42824691f17affd1bd0c525a721
arosha619 <90087992+arosha619@users.noreply.github.com>
2022-12-20 22:06:02 +0530

    add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
+import "../layouts/index.css"
 
 const Indexpage = () =>{
   return(
     <div>
-      helo
+     <div className="Hero">
+      <div className="heroGroup">
+        <h1>Hello welcome to Cording World!</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+        <Link to="/page-2">Watch the video</Link>
+
+      </div>
+
+     </div>
     </div>
   )
 }
 export default Indexpage
 
-export const Head = () => <title>Arosha Sandaruwan</title>
+export const Head = () => <title>Cording World</title>
